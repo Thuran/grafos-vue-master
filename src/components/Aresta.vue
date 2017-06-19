a54a0b9f89c75173a7ac1139847f2ada7a1e33ab
Thuran <thuran_eduardo@hotmail.com>
2017-06-18 22:01:49 -0300

    first commit

---
+++ src/components/Aresta.vue
@@ -0,0 +1,105 @@
+<template>
+    <g :transform="transform">
+        <defs>
+            <marker id="markerArrow" markerWidth="13" markerHeight="13" refX="2" refY="6" orient="auto">
+                 <path d="M2,2 L2,11 L10,6 L2,2" style="fill: #000;" />
+            </marker>
+            <path :id="id" :d="path" :style="{ 'stroke': color, 'stroke-width': '1' }" marker-mid="url(#markerArrow)"/>
+        </defs>
+        <use :xlink:href="refId" fill="none"/>
+        <text text-anchor="middle" fill="black" v-on:dblclick="consol">
+            <textPath startOffset="50%" :xlink:href="refId">{{flow}}{{pesoCorreto}}</textPath>
+        </text>
+        <text fill="white">
+            <textPath :xlink:href="refId" startOffset="30%" style="dominant-baseline: central; ">➤</textPath>
+            <textPath :xlink:href="refId" startOffset="70%" style="dominant-baseline: central; ">➤</textPath>
+        </text>
+    </g>
+</template>
+
+<script>
+export default {
+    name: 'aresta',
+    data() {
+        return {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 0,
+            id: 0,
+            tx: 0,
+            ty: 0,
+            ligacoes: 0,
+            color: 'rgb(219, 219, 219)',
+            peso: 0,
+            flow: ""
+        }
+    },
+
+    created() {
+        let self = this;
+        self.x1 = self.from.x + 25;
+        self.y1 = self.from.y - 50;
+        self.x2 = self.to.x + 25;
+        self.y2 = self.to.y - 50;
+        self.id = "aresta-" + self.arestaId;
+        self.ligacoes = self.lig;
+        self.consol();
+    },
+
+    watch: {
+        'from.x'(value) {
+            let self = this;
+            self.x1 = self.from.x + 25;
+        },
+        'from.y'(value) {
+            let self = this;
+            self.y1 = self.from.y - 50;
+        },
+        'to.x'(value) {
+            let self = this;
+            self.x2 = self.to.x + 25;
+        },
+        'to.y'(value) {
+            let self = this;
+            self.y2 = self.to.y - 50;
+        }
+    },
+
+    computed: {
+        path() {
+            let self = this;
+            return "M " + self.x1 + " " + self.y1 + " l " + (self.x2 - self.x1) + " " + (self.y2 - self.y1);
+        },
+        refId(){
+            let self = this;
+            return "#" + self.id;
+        },
+        pesoCorreto(){
+            let self = this;
+            if(self.peso === 0){
+                return "";
+            }else{
+                return self.peso;
+            }
+        },
+        transform() {
+            console.log(this);
+            return `translate(${this.movey}, ${this.movex})`
+        }
+    },
+
+    methods: {
+        consol(){
+            let self = this;
+            var result = prompt("Insira o peso da aresta", "0");
+            self.peso = result;
+        }
+    },
+
+    props: ['aresta-id', 'from', 'to', 'lig', 'movex', 'movey']
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,81 @@
+<template>
+<div class="fill" style="background-color: grey;">
+      <nav class="nav" style="background-color: grey;">
+            <div class="nav-center">
+                <div class="nav-item">
+                  <button class="button" style="background-color: black; color: white" v-bind:class="{ 'is-active': 1 == selected }" @click="select(1)">Adicionar Vertice</button>
+                  <button class="button" style="background-color: black; color: white" v-bind:class="{ 'is-active': 2 == selected }" @click="select(2)">Adicionar Aresta</button>
+                  <button class="button" style="background-color: black; color: white" v-bind:class="{ 'is-active': 3 == selected }" @click="select(3)">Mover</button>
+                  <button class="button is-danger is-outlined" v-bind:class="{ 'is-active': 4 == selected }" @click="select(4)">Remover</button>
+                  <button class="button" style="background-color: black; color: white" v-bind:class="{ 'is-active': 5 == selected }" @click="select(5)">Welsh e Powell</button>
+                  <button class="button" style="background-color: black; color: white" v-bind:class="{ 'is-active': 6 == selected }" @click="select(6)">DSATUR</button>
+                  <button class="button" style="background-color: black; color: white"  v-bind:class="{ 'is-active': 7 == selected }" @click="select(7)">Prim</button>
+                  <button class="button" style="background-color: black; color: white" v-bind:class="{ 'is-active': 8 == selected }" @click="select(8)">Ford-Fulkerson</button>
+                  <button class="button" style="background-color: orange; color: white" v-bind:class="{ 'is-active': 9 == selected }" @click="select(9)">Limpar</button>
+                </div>
+            </div>
+        </nav>
+        <p style="text-align: center">{{msg}}</p>
+        <board :btnSelected="selected"></board>
+        </div>
+</template>
+
+<script>
+
+import Board from './Board';
+
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      selected: '',
+      msg: ''
+    }
+  },
+  methods: {
+    select(number){
+      let self = this;
+      self.selected = number;
+            switch(number){
+        case 1:
+          self.msg = "Clique na tela para adicionar vertices!";
+          break;
+        case 2:
+          self.msg = "Clique no vertice inicial e depois clique novamente no final para adicionar a aresta!";
+          break;
+        case 3:
+          self.msg = "Clique no vertice e segure para mover!";
+          break;
+        case 4:
+          self.msg = "Clique no vertice para remover!";
+          break;
+        case 5:
+          self.msg = "Clique no espaço em branco para executar o algoritmo Wesh e Powell!";
+          break;
+        case 6:
+          self.msg = "Clique no espaço em branco para executar o algoritmo DSATUR!";
+          break;
+        case 7:
+          self.msg = "Clique no espaço em branco para executar o algoritmo PRIM!";
+          break;
+        case 8:
+          self.msg = "Clique no espaço em branco para executar o algoritmo Ford Fulkerson!";
+          break 
+        case 9:
+          location.reload();
+          break;
+      }
+    }
+  },
+  components: {
+    Board
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .fill{
+    height: 100%;
+  }
+</style>
